14b9ee605af4ad697b3a65b6c34aa130c440b714
David Deal <ddeal@linuxfoundation.org>
2023-01-30 11:12:06 -0800

    Updated to Angular 12
    Signed-off-by: David Deal <ddeal@linuxfoundation.org>

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,7 +63,7 @@ import '@angular/localize/init';
  * Zone JS is required by default for Angular itself.
  */
 import '@angular/localize/init'
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
