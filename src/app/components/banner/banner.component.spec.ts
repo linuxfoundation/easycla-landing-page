0874cc52784cb58a84b9a15fa23f539512b16d56
ahmedomosanya <aopeyemi@contractor.linuxfoundation.org>
2025-12-19 15:18:22 +0100

    test: add BannerComponent spec file
    Closes: https://github.com/linuxfoundation/easycla/issues/4892
    Signed-off-by: ahmedomosanya <aopeyemi@contractor.linuxfoundation.org>

---
+++ src/app/components/banner/banner.component.spec.ts
@@ -0,0 +1,25 @@
+// Copyright The Linux Foundation and each contributor to CommunityBridge.
+// SPDX-License-Identifier: MIT
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BannerComponent } from './banner.component';
+
+describe('BannerComponent', () => {
+  let component: BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BannerComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BannerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
